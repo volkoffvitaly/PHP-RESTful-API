a6c00e4eadfc1dd0b3d45656f6d6806e2aaf730c
MrDante <firstmrdante@gmail.com>
2020-05-04 13:44:16 +0700

    Base files added

---
+++ frontend/precache-manifest.691e4e46a5b0625940df83b7bf4eb98d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efd21ee4fcf855afd76a6632b5305407",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2edadff240908c6121e",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "71ef36f7a5f146b1c2f1",
+    "url": "/static/css/main.62156323.chunk.css"
+  },
+  {
+    "revision": "d2edadff240908c6121e",
+    "url": "/static/js/2.86b40839.chunk.js"
+  },
+  {
+    "revision": "71ef36f7a5f146b1c2f1",
+    "url": "/static/js/main.0ed8dd9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
